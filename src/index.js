1632e12e71719c332807ba6bff5ba1342bb80327
Дмитрий Ткаченко <dmitrytkachenko7397@gmail.com>
2023-04-27 11:00:55 +0200

    fix hash

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import store from "redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/test-follow#">
+    <BrowserRouter basename="/test-follow">
       <Provider store={store}>
         <App />
       </Provider>
